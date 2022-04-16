77db44979022a517767c45490ec97b4e1670908c
atomyongya <yongyaatom@github.com>
2022-04-16 09:30:55 +0545

    Cleaning File and Folder

--- program_Code/main.py
+++ program_Code/main.py
@@ -151,92 +151,94 @@ class Numa_VoiceAssistant(object):
                 predicted_keyword = queue_Thread_Prediction.get()
                 print(predicted_keyword)
                 
-                # if count_Outside < 2:
-                #     predicted_keyword = "No"
-                # else:
-                #     predicted_keyword = "numa"
-                #     count_Outside = 0
-                
-                # count_Outside = count_Outside + 1
-                
-                if predicted_keyword == wake_Word:
-                    print("Inside....")
-                    
-                    # Displaying GUI of application if "numa" is activated.
-                    gui_object = frontend.gui_Object
-                    gui_object.display_After_Minimize_Close()
-                    
-                    # Sending the signal as audio to inform user that numa is ready to take command.
-                    mostly_Used_Function.play_Audio(file_Path.wake_Word_Sound_Effect)
-                    
-                    if predicted_keyword.count(wake_Word) > 0:
-                        """
-                        Condition to check the count of wake word is greater then zero to take next command of user.
-                        
-                        :var count (int) : To keep track of below while loop.
-                        """
-                        count = 1
-                        
-                        try: 
-                            while True:
-                                """
-                                Loop to get the user main command that will execute some program.
-                                
-                                :var user_Command (str) : predicted_keyword.
-                                """  
-                                
-                                # thread_Animation = Thread(target=gui_object.animation_If_User_Speak, args=(canvas,))
-                                # thread_Animation.setDaemon(True)
-                                # thread_Animation.start()
-                                
-                                # Creating thread to predict the user command voice.
-                                thread_Prediction1 = Thread(target=self.prediction, args=(queue_Thread_Prediction, lock_Thread_Prediction))
-                                thread_Prediction1.setDaemon(True)
-                                # print("{}".format(thread_Prediction.is_alive()))
-                                thread_Prediction1.start()
-                                thread_Prediction1.join()
-                                
-                                # Stoting the value value of predicted_keyword in user_Command variable.
-                                predicted_keyword = queue_Thread_Prediction.get()
-                                user_Command = predicted_keyword
-                                print("Inside: ",user_Command)
-                                
-                                # Appending the user predicted_keyword/user_Command in list_Of_Word list.
-                                self.list_Of_Word.append(user_Command)
-                                
-                                # Displaying output in GUI of Application.
-                                label_Language_Info.config(text=list_Of_Word, font=(20))
-                                
-                                count = count + 1
-                                queue_Thread_Prediction.task_done()
-                                
-                                # user_Command = "open chrome"
-                                # Condition to break loop after 4 iteration or 4 second because each record time is 1 second. 
-                                if count == 4:
-                                    """
-                                    Program execuation code.
-                                    """
-                                    
-                                    print(self.list_Of_Word)
-                                    
-                                    # To open an application of system.
-                                    if "open" in user_Command:
-                                        application_Name = user_Command.replace("open", "")
-                                        mostly_Used_Function.produce_Voice("Opeaning " + application_Name)
-                                        open_Application_Object = open_Application.Open_Application(application_Name)
-                                        open_Application_Object.open_Application()
-                                    
-                                    
-                                    # Breaking the loop after the execuation of the program.
-                                    break
-                                    
-                        except Exception as error:
-                            print("Error from class main and function main: First exception eror", error)
-                        
-                        self.list_Of_Word.clear()
-            
+                if count_Outside < 2:
+                    predicted_keyword = "No"
                 else:
-                    continue 
+                    predicted_keyword = "numa"
+                    count_Outside = 0
+                
+                count_Outside = count_Outside + 1
+                
+                mostly_Used_Function.save_Awarded_Audio()
+                
+                # if predicted_keyword == wake_Word:
+                #     print("Inside....")
+                    
+                #     # Displaying GUI of application if "numa" is activated.
+                #     gui_object = frontend.gui_Object
+                #     gui_object.display_After_Minimize_Close()
+                    
+                #     # Sending the signal as audio to inform user that numa is ready to take command.
+                #     mostly_Used_Function.play_Audio(file_Path.wake_Word_Sound_Effect)
+                    
+                #     if predicted_keyword.count(wake_Word) > 0:
+                #         """
+                #         Condition to check the count of wake word is greater then zero to take next command of user.
+                        
+                #         :var count (int) : To keep track of below while loop.
+                #         """
+                #         count = 1
+                        
+                #         try: 
+                #             while True:
+                #                 """
+                #                 Loop to get the user main command that will execute some program.
+                                
+                #                 :var user_Command (str) : predicted_keyword.
+                #                 """  
+                                
+                #                 # thread_Animation = Thread(target=gui_object.animation_If_User_Speak, args=(canvas,))
+                #                 # thread_Animation.setDaemon(True)
+                #                 # thread_Animation.start()
+                                
+                #                 # Creating thread to predict the user command voice.
+                #                 thread_Prediction1 = Thread(target=self.prediction, args=(queue_Thread_Prediction, lock_Thread_Prediction))
+                #                 thread_Prediction1.setDaemon(True)
+                #                 # print("{}".format(thread_Prediction.is_alive()))
+                #                 thread_Prediction1.start()
+                #                 thread_Prediction1.join()
+                                
+                #                 # Stoting the value value of predicted_keyword in user_Command variable.
+                #                 predicted_keyword = queue_Thread_Prediction.get()
+                #                 user_Command = predicted_keyword
+                #                 print("Inside: ",user_Command)
+                                
+                #                 # Appending the user predicted_keyword/user_Command in list_Of_Word list.
+                #                 self.list_Of_Word.append(user_Command)
+                                
+                #                 # Displaying output in GUI of Application.
+                #                 label_Language_Info.config(text=list_Of_Word, font=(20))
+                                
+                #                 count = count + 1
+                #                 queue_Thread_Prediction.task_done()
+                                
+                #                 # user_Command = "open chrome"
+                #                 # Condition to break loop after 4 iteration or 4 second because each record time is 1 second. 
+                #                 if count == 4:
+                #                     """
+                #                     Program execuation code.
+                #                     """
+                                    
+                #                     print(self.list_Of_Word)
+                                    
+                #                     # To open an application of system.
+                #                     if "open" in user_Command:
+                #                         application_Name = user_Command.replace("open", "")
+                #                         mostly_Used_Function.produce_Voice("Opeaning " + application_Name)
+                #                         open_Application_Object = open_Application.Open_Application(application_Name)
+                #                         open_Application_Object.open_Application()
+                                    
+                                    
+                #                     # Breaking the loop after the execuation of the program.
+                #                     break
+                                    
+                #         except Exception as error:
+                #             print("Error from class main and function main: First exception eror", error)
+                        
+                #         self.list_Of_Word.clear()
+            
+                # else:
+                #     continue 
                 
         except Exception as error:
             print("Error in class main and Function main: Second exception error", error)
